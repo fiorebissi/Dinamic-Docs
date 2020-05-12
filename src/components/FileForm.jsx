a1531f719cf1bc5a55b49067a6006ca0e55b18d6
Fiorella Bissi <fiorellabissi@hotmail.com>
2020-05-12 19:09:27 -0300

    Agregando rutas

--- src/components/FileForm.jsx
+++ src/components/FileForm.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 
 const FileForm = () => {
+
+  const handleClick = () => {
+    const header = { method: 'POST',
+      body: document.querySelector('#file').files[0],
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    // credentials: 'include',
+    };
+    fetch('http://www.rchdynamic.com.ar/dd/document/create/excel', header)
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then((response) => {
+        console.log(response);
+      });
+
+  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log('Hola');
+  // };
   return (
     <main className='bg-white w-full h-full flex flex-col items-center content-center justify-center min-h-screen'>
-      <input className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='file' name='' id='' />
-      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='button'>Enviar</button>
+      <button onClick={handleClick} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='button'>Enviar</button>
+      <form method='POST' action='http://www.rchdynamic.com.ar/dd/document/create/excel'>
+        <input className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='file' name='file' id='file' />
+        <button type='submit'>Enviar</button>
+
+      </form>
     </main>
 
   );
 };
+
+// onSubmit={(e) => handleSubmit(e)}
 
 export default FileForm;
--- src/components/FormClient.jsx
+++ src/components/FormClient.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 
 const FormClient = () => {
+
+  const handleDownload = () => {
+    const header = { method: 'POST',
+      body: JSON.stringify({
+        'name_template': 'mailing',
+        'last_name': document.getElementById('lastname').value,
+        'first_name': document.getElementById('firstname').value,
+        'email': document.getElementById('email').value,
+        'enterprise': document.getElementById('empresa').value,
+        'is_download_file': false,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // credentials: 'include',
+    };
+    fetch('http://www.rchdynamic.com.ar/dd/document/create', header)
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then((response) => {
+        console.log(response);
+      });
+
+  };
   return (
     <main className='pt-8 w-full h-full items-center flex flex-col justify-center min-w-full min-h-full'>
       <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
         <div className='mb-4'>
           <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
             Nombre
-            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id='name' type='text' placeholder='Nombre' />
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id='firstname' type='text' placeholder='Nombre' />
           </label>
         </div>
         <div className='mb-6'>
@@ -32,7 +60,7 @@ const FormClient = () => {
           <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='button'>
             Descargar
           </button>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='button'>
+          <button onClick={handleDownload} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='button'>
             Enviar
           </button>
         </div>
