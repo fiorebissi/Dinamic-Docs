6e45ffdb724f1c69bf829ab81faa8209d2b2f753
Fiorella Bissi <fiorellabissi@hotmail.com>
2020-05-15 13:08:39 -0300

    Mejoras en tabla FileForm

--- src/components/FileForm.jsx
+++ src/components/FileForm.jsx
@@ -4,7 +4,7 @@ const FileForm = () => {
 
   const [dataDOM, setDataDOM] = useState(null);
 
-  const prueba = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
     const formData = new FormData(document.forms.namedItem('formCsv'));
     const miInit = {
@@ -26,32 +26,33 @@ const FileForm = () => {
     <main className='bg-white w-full h-full flex flex-col items-center content-center justify-center min-h-screen'>
       <form encType='multipart/form-data' method='post' name='formCsv'>
         <input className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='file' name='fileCSV' id='file' required />
-        <button onClick={(e) => prueba(e)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='button' value='Enviar'>Enviar</button>
+        <button onClick={(e) => handleClick(e)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='button' value='Enviar'>Enviar</button>
       </form>
-      {dataDOM && dataDOM.body.list_user.map((data, index) => {
-        const { firstName, lastName, email, enterprise } = data;
-        return (
-          <table className='border-collapse border-2 border-gray-500'>
-            <thead>
-              <tr>
-                <th className='px-4 py-2'>Nombre</th>
-                <th className='px-4 py-2'>Apellido</th>
-                <th className='px-4 py-2'>Email</th>
-                <th className='px-4 py-2'>Empresa</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr key={index.id}>
-                <td className='border px-4 py-2'>{firstName}</td>
-                <td className='border px-4 py-2'>{lastName}</td>
-                <td className='border px-4 py-2'>{email}</td>
-                <td className='border px-4 py-2'>{enterprise}</td>
-              </tr>
-            </tbody>
-          </table>
-        );
-      })}
-
+      {dataDOM && (
+        <table className='border-dotted border-4 border-blue-600 border-opacity-75 rounded-lg shadow-xl'>
+          <thead>
+            <tr>
+              <th className='px-4 py-2'>Nombre</th>
+              <th className='px-4 py-2'>Apellido</th>
+              <th className='px-4 py-2'>Email</th>
+              <th className='px-4 py-2'>Empresa</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataDOM && dataDOM.body.list_user.map((data, index) => {
+              const { firstName, lastName, email, enterprise } = data;
+              return (
+                <tr key={index.id}>
+                  <td className='border px-4 py-2'>{firstName}</td>
+                  <td className='border px-4 py-2'>{lastName}</td>
+                  <td className='border px-4 py-2'>{email}</td>
+                  <td className='border px-4 py-2'>{enterprise}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </main>
 
   );
