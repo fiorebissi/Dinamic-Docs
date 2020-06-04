5bd15423b1584890e4f23b47c6c81daa9b6271ae
Fiorella Bissi <fiorellabissi@hotmail.com>
2020-06-04 17:38:22 -0300

    Cambios FileForm

---
+++ src/components/FileDD.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const FileDD = ({ setDataMailing }) => {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const formData = new FormData(document.forms.namedItem('formCsv'));
+    const miInit = {
+      method: 'POST',
+      body: formData,
+      // credentials: 'include',
+    };
+    fetch(`http://www.rchdynamic.com.ar/dd/document/excel/${template}`, miInit)
+    // fetch('http://localhost:3000/dd/document/create/excel', miInit)
+      .then((response) => response.json())
+      .catch((error) => console.error('Error:', error))
+      .then((response) => {
+        console.log('Success:', response);
+        setDataDOM(response);
+      });
+  };
+  return (
+    <form encType='multipart/form-data' method='post' name='formCsv'>
+      <input className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='file' name='fileCSV' id='file' required />
+      <div className='text-center'>
+        <button onClick={(e) => handleClick(e)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline' type='button' value='Enviar'>Generar</button>
+      </div>
+    </form>
+  );
+};
+
+export default FileDD;
